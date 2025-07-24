3d2ecd736db7d2c825fdb14985e1b3375ea581dc
Suryansh <suryanshvaish1@gmail.com>
2025-07-24 23:34:49 +0530

    fixed in docs/page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -18,16 +18,16 @@ export default function PageDocs() {
     {
       id: 1,
       question: "Why copy/paste and not packaged as a dependency",
-      answer: `The goal of Drift UI is to give developers complete ownership and flexibility over their components. By copying the code directly into your project, you’re free to customize the components to meet specific design needs without being restricted by pre-packaged styles or dependencies.
+      answer: `The goal of Drift UI is to give developers complete ownership and flexibility over their components. By copying the code directly into your project, you&#39;re free to customize the components to meet specific design needs without being restricted by pre-packaged styles or dependencies.
 
-  Starting with customizable defaults, you can adapt each component to align with your projects unique style and requirements.
+Starting with customizable defaults, you can adapt each component to align with your project&#39;s unique style and requirements.
 
-  One of the limitations of npm packages is that design can often be tightly coupled with the component implementation. With Atomix UI, you maintain separation between design and implementation, allowing for more creative control.`,
+One of the limitations of npm packages is that design can often be tightly coupled with the component implementation. With Atomix UI, you maintain separation between design and implementation, allowing for more creative control.`,
     },
     {
       id: 2,
       question: `Which frameworks are supported?`,
-      answer: `Currently, our project supports Next.js out of the box. We're optimized for seamless integration with the Next.js ecosystem, ensuring the best developer experience and performance. Support for additional frameworks may be added in the future based on community demand.`,
+      answer: `Currently, our project supports Next.js out of the box. We&#39;re optimized for seamless integration with the Next.js ecosystem, ensuring the best developer experience and performance. Support for additional frameworks may be added in the future based on community demand.`,
     },
     {
       id: 3,
@@ -35,6 +35,7 @@ export default function PageDocs() {
       answer: `No, there are no plans to release Drift UI as an npm package. The intention is for developers to take ownership of the components and adapt them as they see fit.`,
     },
   ];
+
   return (
     <>
       <div className="flex flex-col min-h-screen sm: mx-0 lg:mx-26 sm: max-w-full lg:w-4xl">
@@ -44,7 +45,7 @@ export default function PageDocs() {
             onClick={srcollService}
             className="text-gray-600 border-l border-gray-500 pl-2 py-1 mt-1 cursor-pointer hover:text-black text-sm"
           >
-            FAQ's
+            FAQ&#39;s
           </div>
         </div>
         <div className="flex flex-col sm: px-5 lg:px-20">
@@ -63,22 +64,22 @@ export default function PageDocs() {
             </div>
             <div className="mt-6 text-md font-sans text-gray-600">
               This is <span className="text-black font-semibold">NOT</span> a
-              traditional component library. Instead, its a toolkit of reusable
-              components meant to empower you to create a unique, scalable UI
-              without dependency constraints.
+              traditional component library. Instead, it&#39;s a toolkit of
+              reusable components meant to empower you to create a unique,
+              scalable UI without dependency constraints.
             </div>
             <div className="text-black font-sans text-[18px] mt-12">
               What do you mean by not a component library?
             </div>
-            <div className="mt-4 text-md font-sans text-gray-600  text-left">
+            <div className="mt-4 text-md font-sans text-gray-600 text-left">
               Unlike traditional libraries, Drift UI is not available as an
-              installable npm package. You wont add it as a dependency; instead,
-              you choose and integrate only the components you need by copying
-              and pasting them into your codebase. This approach gives you full
-              control over customization, so the components truly feel like part
-              of your application.
+              installable npm package. You won&#39;t add it as a dependency;
+              instead, you choose and integrate only the components you need by
+              copying and pasting them into your codebase. This approach gives
+              you full control over customization, so the components truly feel
+              like part of your application.
             </div>
-            <div className="mt-16 text-[17px] font-sans text-gray-600 ">
+            <div className="mt-16 text-[17px] font-sans text-gray-600">
               Use DRIFT UI as a foundation to build your own tailored component
               libraries.
             </div>
@@ -94,7 +95,7 @@ export default function PageDocs() {
         </div>
         <div className="sm: px-8 lg:px-22 mt-32 mb-12 flex flex-col">
           <div ref={faqRef} className="text-2xl font-semibold">
-            FAQ's
+            FAQ&#39;s
           </div>
 
           {faq.map((q) => (
